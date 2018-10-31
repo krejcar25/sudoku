5a0d83a0e2caee6d9a491b90637b39e13e34ff69
Michal Krejci <michal.krejci@outlook.cz>
2018-10-31 13:39:21 +0100

    Fix Color

--- Sudoku/src/cz/krejcar25/sudoku/style/ButtonStyle.java
+++ Sudoku/src/cz/krejcar25/sudoku/style/ButtonStyle.java
@@ -1,8 +1,8 @@
 package cz.krejcar25.sudoku.style;
 
 public class ButtonStyle {
-    public Color border = new Color((byte)0);
+    public Color border = new Color(0);
     public Thickness borderThickness = new Thickness(2);
-    public Color background = new Color((byte)51);
-    public Color foreground = new Color((byte)254);
+    public Color background = new Color(51);
+    public Color foreground = new Color(255);
 }
--- Sudoku/src/cz/krejcar25/sudoku/style/Color.java
+++ Sudoku/src/cz/krejcar25/sudoku/style/Color.java
@@ -3,31 +3,31 @@ package cz.krejcar25.sudoku.style;
 import processing.core.PGraphics;
 
 public class Color {
-    public byte r;
-    public byte g;
-    public byte b;
-    public byte a;
+    public int r;
+    public int g;
+    public int b;
+    public int a;
 
-    public Color(byte gray) {
+    public Color(int gray) {
         this.r = gray;
         this.g = gray;
         this.b = gray;
     }
 
-    public Color(byte gray, byte a) {
+    public Color(int gray, int a) {
         this.r = gray;
         this.g = gray;
         this.b = gray;
         this.a = a;
     }
 
-    public Color(byte r, byte g, byte b) {
+    public Color(int r, int g, int b) {
         this.r = r;
         this.g = g;
         this.b = b;
     }
 
-    public Color(byte r, byte g, byte b, byte a) {
+    public Color(int r, int g, int b, int a) {
         this.r = r;
         this.g = g;
         this.b = b;

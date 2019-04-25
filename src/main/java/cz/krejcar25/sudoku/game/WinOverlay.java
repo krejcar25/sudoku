9098efca44c17c5dc6faca103b4cc432405b38cc
Michal Krejčí <michal@krejci.vip>
2019-04-25 22:31:06 +0200

    Removed time text from WinOverlay
    It is redundant since user can still see it in the GameView

--- src/main/java/cz/krejcar25/sudoku/game/WinOverlay.java
+++ src/main/java/cz/krejcar25/sudoku/game/WinOverlay.java
@@ -8,15 +8,13 @@ import cz.krejcar25.sudoku.ui.control.Button;
 import processing.core.PApplet;
 
 class WinOverlay extends BaseOverlay {
-	private final Clock clock;
-
 	WinOverlay(BaseView baseView, BaseGrid grid) {
 		super(baseView, PApplet.constrain((baseView.width - 540) / 2, 0, baseView.width - 540), (int) (grid.getCore().ncr * grid.getSy() / 2) - 100, PApplet.constrain(baseView.width, 0, 540), 200, OverlayType.OK, (sender) ->
 		{
 			System.out.println("WinOverlay OK button has received click event, popping");
 			baseView.getViewStack().pop(2);
 		});
-		clock = grid.getGameClock();
+		Clock clock = grid.getGameClock();
 
 		getRootApplet().scoreboard.addEntry(grid.getGridProperties(), grid.getGridDifficulty(), clock.getTimer().getStartDate(), clock.getTimer().getElapsedTime());
 	}
@@ -31,9 +29,6 @@ class WinOverlay extends BaseOverlay {
 		textSize(40);
 		textAlign(PApplet.CENTER, PApplet.CENTER);
 		text("You have won!", width / 2f, 60);
-		textSize(20);
-		textAlign(LEFT, TOP);
-		text(String.format("It took you %d minutes and %d seconds to complete this grid.", clock.getTimer().getMinutes(), clock.getTimer().getSeconds()), 20, 90, width - 40, 190);
 		drawButtons();
 	}
 
